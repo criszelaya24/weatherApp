84608e4c9d43627b16f7850122d2bfe4ce6eda4d
cristopher Palacios <cristopher@fanatiz.com>
2020-08-07 12:50:01 -0400

    Setting up static views

--- src/app.js
+++ src/app.js
@@ -1,6 +1,39 @@
 const geocode = require('./utils/geocode');
 const express = require('express'),
-    app = express();
+    app = express(),
+    hbs = require('hbs'),
+    path = require('path');
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+// Static views
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'WeatherApp',
+        name: 'Cristopher Palacios',
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Cristopher Palacios',
+    });
+});
+
+// API view
 
 app.get('/weather', (req, res) => {
     const address = req.query.address; 
@@ -19,8 +52,10 @@ app.get('/weather', (req, res) => {
 });
 
 app.all('*', (req, res) => {
-    res.send({
-        data: 'Endpoint does not exist',
+    res.render('404', {
+        title: '404',
+        name: 'Cristopher Palacios',
+        errorMessage: 'Page not found.',
     });
 });
 
